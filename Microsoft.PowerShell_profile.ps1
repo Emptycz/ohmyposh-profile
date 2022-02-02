86fa53657a3dd5c165320d555c51ce1422b4dbb5
Jan Kočvara <kimichisxd285@gmail.com>
2022-02-02 12:38:26 +0100

    Added search functions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,18 @@
+# Load prompt config
 oh-my-posh --init --shell pwsh --config C:\Users\kimic\AppData\Local\Programs\oh-my-posh\themes/theempty.omp.json | Invoke-Expression
+
+#Icons
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+Set-PsReadLineOption -PredictionViewStyle ListView
+
+# Alias
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias clr clear 
