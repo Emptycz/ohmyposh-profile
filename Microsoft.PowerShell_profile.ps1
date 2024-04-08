f32ff5a531f79fd517107803caebe6e2b25c78ef
Jan Kočvara <kimichisxd285@gmail.com>
2024-04-08 10:31:46 +0200

    minimalistic ohmyposh profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,8 @@
 # Load prompt config
-oh-my-posh --init --shell pwsh --config C:\Users\kimic\AppData\Local\Programs\oh-my-posh\themes/theempty.omp.json | Invoke-Expression
+# oh-my-posh --init --shell pwsh --config C:\Users\kimic\AppData\Local\Programs\oh-my-posh\themes/theempty.omp.json | Invoke-Expression
+# oh-my-posh --init --shell pwsh --config C:\Users\kimic\AppData\Local\Programs\oh-my-posh\themes/kushal.omp.json | Invoke-Expression
+# oh-my-posh --init --shell pwsh --config C:\Users\kimic\AppData\Local\Programs\oh-my-posh\themes/tokyonight_storm.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config C:\Users\kimic\AppData\Local\Programs\oh-my-posh\themes/theempty.star.omp.json | Invoke-Expression
 
 #Icons
 Import-Module -Name Terminal-Icons
@@ -15,4 +18,18 @@ Set-PsReadLineOption -PredictionViewStyle ListView
 Set-Alias ll ls
 Set-Alias g git
 Set-Alias grep findstr
-Set-Alias clr clear 
+Set-Alias clr clear
+
+# On command 'vs' open Visual Studio project in current folder
+function vs {
+    Get-ChildItem *.sln -Recurse | Invoke-Item
+}
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
